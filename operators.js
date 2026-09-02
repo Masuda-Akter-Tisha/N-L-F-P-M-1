3493806dcf7d303d66a2e278851eed81301b546a
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-09-02 08:49:19 +0600

    explore various operators

---
+++ operators.js
@@ -0,0 +1,24 @@
+//Arithmetic operators
+const a = 10;
+const b = 3;
+
+const sum = a+b;
+// console.log(sum);
+
+const division = a / b;
+// console.log(division.toFixed (2));
+
+const remainder = a%b;
+console.log(remainder);
+
+// a + b - (c + f) * d / e
+
+// Assignment operators
+//  sum = sum + 100;  sum += 100;
+
+// comparison Operators
+console.log(5  >= 5);
+
+// logical operators
+const c =  (a > b) && 10;
+console.log(c);
