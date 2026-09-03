3efd012671a53c4b51154600f9b5c7e510b4b35c
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-09-03 08:47:40 +0600

    explore strings

---
+++ strings.js
@@ -0,0 +1,17 @@
+let firstName = 'Masuda';
+let lastName = 'Tisha';
+
+// const fullName = firstName + " " + lastName;
+let fullName = `${firstName}  ${lastName} \nBrahmanbaria`;
+console.log(fullName);
+
+let randomStr = "Iam a \"student\" ";
+let student = 'It\'s a student id';
+let address = 'Brahmanbaria , \'kalibarir Moor\'';
+console.log(randomStr, student, address);
+
+let flower = 'rose';
+// console.log(flower.toUpperCase ());
+// console.log(flower.toLowerCase ());
+console.log(randomStr.includes ("\""));
+console.log(address.length);
