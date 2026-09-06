8eb82b17ab756fee91633dff2a4ed3ae180f27bc
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-09-06 09:46:56 +0600

    explore truthy false values

---
+++ values.js
@@ -0,0 +1,12 @@
+let age = 0;
+if (age) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+const mul = age * 'hello';
+console.log(mul);
+
+console.log(typeof NaN);
+console.log(typeof null);
